e727b186c01139618fd56d07075d1bab8f8a25c1
hassankhairi <hkhairi@ucsd.edu>
2022-02-05 01:12:34 -0800

    example

--- app/src/main/java/com/example/birdsofafeather/MainActivity.java
+++ app/src/main/java/com/example/birdsofafeather/MainActivity.java
@@ -1,31 +1,20 @@
 package com.example.birdsofafeather;
 
-import static android.widget.ImageView.*;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Looper;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
     Handler mainHandler = new Handler();
-    ImageView imageView; // new
-    TextView URLText; // new
+    ImageView imageView;
+    TextView URLText;
 
 
     @Override
@@ -35,54 +24,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onConfirmClicked(View view) {
+
     }
 
-    // new
     public void onDoneClicked(View view){
         imageView = findViewById(R.id.pfp);
         URLText = findViewById(R.id.URL);
         String URLString = URLText.getText().toString();
         Glide.with(this).load(URLString).into(imageView);
     }
-
-
-/*
-    public void onDoneClicked(View view) throws IOException {
-        TextView URLText = findViewById(R.id.URL);
-        ImageView img = findViewById(R.id.pfp);
-        String s = URLText.getText().toString();
-
-        new FetchImage(s, img).start();
-    }
-
-    class FetchImage extends Thread{
-        String URL;
-        Bitmap btp;
-        ImageView image;
-
-        FetchImage(String URL, ImageView image){
-            this.URL = URL;
-            this.image = image;
-
-        }
-
-        @Override
-        public void run(){
-            InputStream in = null;
-            try {
-                in = new URL(URL).openStream();
-                btp = BitmapFactory.decodeStream(in);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            mainHandler.post(new Runnable() {
-                @Override
-                public void run(){
-                    image.setImageBitmap(btp);
-                }
-            });
-        }
-    } */
-
-
 }
