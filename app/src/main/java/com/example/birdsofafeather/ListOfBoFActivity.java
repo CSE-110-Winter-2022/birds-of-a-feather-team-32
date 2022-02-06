a9773950cc7f4c9852ae80d50c685ce9681d3d5e
Matthew Peng <MP@Matts-Mac.local>
2022-02-05 17:11:32 -0800

    added nearby messages support

--- app/src/main/java/com/example/birdsofafeather/ListOfBoFActivity.java
+++ app/src/main/java/com/example/birdsofafeather/ListOfBoFActivity.java
@@ -1,12 +1,19 @@
 package com.example.birdsofafeather;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.birdsofafeather.model.Student;
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
 
 import java.util.Arrays;
 
@@ -32,6 +39,11 @@ public class ListOfBoFActivity extends AppCompatActivity {
             new Student("Student E")
     };
 
+    private static final String TAG = "bofNearby";
+    private MessageListener realListener;
+    private FakedMessageListener testListener;
+    private int buttonState = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,5 +56,39 @@ public class ListOfBoFActivity extends AppCompatActivity {
 
         studentViewAdapter = new ListOfBoFViewAdapter(Arrays.asList(data));
         studentRecyclerView.setAdapter(studentViewAdapter);
+
+        realListener = new MessageListener() {
+            @Override
+            public void onFound(@NonNull Message message) {
+                Log.d(TAG, "Found message: " + new String(message.getContent()));
+            }
+
+            @Override
+            public void onLost(@NonNull Message message) {
+                Log.d(TAG, "Lost sight of message: " + new String(message.getContent()));
+            }
+        };
+        this.testListener = new FakedMessageListener(realListener, 3, "Hello World");
     }
+
+    public void onStartClicked(View view) {
+        Button startButton = findViewById(R.id.runButton);
+        if (buttonState == 0) {
+            buttonState = 1;
+            startButton.setText("Stop");
+            Nearby.getMessagesClient(this).subscribe(realListener);
+            testListener.getMessage();
+        } else {
+            buttonState = 0;
+            startButton.setText("Start");
+            Nearby.getMessagesClient(this).unsubscribe(realListener);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Nearby.getMessagesClient(this).unsubscribe(realListener);
+    }
+
 }
